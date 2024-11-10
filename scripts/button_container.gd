f1842c278a9469db51565ef5391bb0c24427a554
Trailblazer Isaiah <123408229+Trailblazer4@users.noreply.github.com>
2024-11-10 10:18:33 -0800

    title disappears now

--- scripts/button_container.gd
+++ scripts/button_container.gd
@@ -6,6 +6,7 @@ func _on_play_button_pressed():
 	#get_tree().change_scene_to_packed(MAIN)
 	visible = false
 	get_parent().get_parent().get_node("Cameraman").stay = false
+	get_parent().get_node("TextureRect").visible = false
 	#GameData.player.get_node("Camera3D").current = true
 
 func _on_quit_button_pressed():
